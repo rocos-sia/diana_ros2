6fab9a53a5e4a8dd227401b4c0d372abbcda5daa
thinkexist1989 <992534544@qq.com>
2025-02-09 23:30:18 +0800

    Update

--- diana_description/launch/display.launch.py
+++ diana_description/launch/display.launch.py
@@ -1,35 +1,44 @@
 import os
 from launch import LaunchDescription
-from launch . substitutions import LaunchConfiguration
-from launch_ros . actions import Node
-from launch_ros . substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import xacro
 
 
 def generate_launch_description():
-    package_name = 'diana7_description'
-    urdf_name = "diana_v2.urdf"
+    package_name = 'diana_description'
+    # urdf_name = "diana_v2.urdf"
     ld = LaunchDescription()
-    pkg_share = FindPackageShare(package=package_name). find(package_name)
-    urdf_model_path = os . path . join(pkg_share, f'urdf/{urdf_name }')
+    pkg_share = FindPackageShare(package=package_name).find(package_name)
+
+    rviz_file = os.path.join(pkg_share, 'rviz/diana7.rviz')
+
+    diana_xacro_path = os.path.join(pkg_share, 'urdf/diana7.urdf.xacro')
+    diana7_description = xacro.process_file(diana_xacro_path).toprettyxml(indent="  ")
+
+    # urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        arguments=[urdf_model_path]
+        parameters=[{"robot_description": diana7_description}],
     )
-    # joint_state_publisher_node = Node (
-    # package ='joint_state_publisher_gui',
-    # executable ='joint_state_publisher_gui',
-    # name ='joint_state_publisher_gui',
-    # arguments =[ urdf_model_path ]
-    # )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        # arguments=[diana7_description],
+    )
 
     rviz2_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
+        arguments=["--display-config", rviz_file]
     )
-    ld . add_action(robot_state_publisher_node)
-    # ld . add_action ( joint_state_publisher_node )
-    ld . add_action(rviz2_node)
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(joint_state_publisher_node)
+    ld.add_action(rviz2_node)
     return ld
